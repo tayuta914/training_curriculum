642ed8a2ed70499a550f85dc4e46d8ea222b862e
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-09-11 17:56:25 +0900

    Revert "修正"
    This reverts commit 8f8e2c922e70ed922881c5e5366a9de2f021c663.

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -34,16 +34,7 @@ class CalendarsController < ApplicationController
       plan = plans.map do |plan|
         today_plans.push(plan.plan) if Plan.date == @todays_date + x
       end
-
-      wday_num = Date.today.wday + x
-       #Date.today.wdayを利用して添字となる数値を得る
-      #もしもwday_numが7以上であれば、7を引く
-      if wday_num >= 7
-        wday_num = wday_num - 7
-      end
-
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[(@todays_date+x).wday]}
-
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, :plans => today_plans}
       @week_days.push(days)
     end
   end
